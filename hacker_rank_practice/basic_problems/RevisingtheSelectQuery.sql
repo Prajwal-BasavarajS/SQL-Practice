4f02971b4aaeff50bfbb430e8b81d02794521a95
Prajwal-BasavarajS <prajwalbasavaraj13@gmail.com>
2026-03-31 12:44:12 +0200

    inital commit

---
+++ hacker_rank_practice/basic_problems/RevisingtheSelectQuery.sql
@@ -0,0 +1,6 @@
+SELECT * from city 
+where COUNTRYCODE = "USA" AND
+population > 100000;
+
+
+-- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
